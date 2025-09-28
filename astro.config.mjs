e0e32ab092b39cf7b0d0947a31daf79d1e65876f
mattnicholas365 <148034550+mattnicholas365@users.noreply.github.com>
2025-09-28 21:07:13 +1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://compliance-365.github.io/website/,
+  site: 'https://compliance-365.github.io/website',
   outDir: 'dist',
   integrations: [sitemap()],
   build: { format: 'directory' }
